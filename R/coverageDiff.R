e1c79ad1b52803029c937d780b8a9cfc303576c0
oreina <oreina@biocf06.irb.pcb.ub.es>
2018-05-31 18:01:30 +0200

    buf fixing

--- R/coverageDiff.R
+++ R/coverageDiff.R
@@ -2,10 +2,12 @@ coverageDiff <- function(sample1, sample2, chrLength) {
   sample1 <- sample1[names(sample1) %in% names(chrLength)]
   sample2 <- sample2[names(sample2) %in% names(chrLength)]
   chrLengthSel <- as.list(chrLength[names(sample1)]); chrLengthSel <- chrLengthSel[!is.na(chrLengthSel)]
-  cover1 <- coverage(sample1, width=chrLengthSel)
+  #cover1 <- coverage(sample1, width=chrLengthSel)
+  cover1 <- coverage(as(sample1,'GRanges'), width=NULL)
   chrLengthSel <- as.list(chrLength[names(sample2)]); chrLengthSel <- chrLengthSel[!is.na(chrLengthSel)]
-  cover2 <- coverage(sample2, width=chrLengthSel)
-  #
+  #cover2 <- coverage(sample2, width=chrLengthSel)
+  cover2 <- coverage(as(sample2,'GRanges'), width=NULL)
+                                        #
   sel <- names(cover1) %in% names(cover2)
   n <- names(cover1)[sel]
   coverdiff <- cover1[n]-cover2[n]
